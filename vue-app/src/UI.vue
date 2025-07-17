a4cbc73d9193091eca9912167be92c0b60cd2893
Heh556 <hengruihrb@gmail.com>
2025-07-16 17:18:51 -0700

    Vue init

---
+++ vue-app/src/UI.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="actions">
+    <button @click="$emit('craft')">Craft</button>
+    <!-- and other buttons  -->
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
+
